62d894ef6a7bc443c512ca9be9f3c591faa2817c
Aman Gupta <techbugaman@gmail.com>
2022-09-05 21:50:15 +0530

    Added routes successfuly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
+import About from "./components/About";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      <Navbar/>
-    </div>
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+        </Routes>
+    </BrowserRouter>
   );
 }
 
