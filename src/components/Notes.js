16326b836f404c8a867a57c46f88734e2359ef9e
Aman Gupta <techbugaman@gmail.com>
2022-09-10 19:25:04 +0530

    Added modal but there is a bug that is notes other than first note aren't getting updated

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,27 +1,78 @@
-import React,{useContext} from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import noteContext from '../context/notes/noteContext';
 
 export default function Notes(props) {
     const notes_available = useContext(noteContext);
-    const {notes,deleteNote}=notes_available;
+    const { notes, deleteNote, updateNote } = notes_available;
+
+    const modal_ui = useRef(null);
+    const [note, setNote] = useState({ eid:"", etitle: "", edescription: "", etag: "" });
+
+
+    const openModal = (currentNote) => {
+        modal_ui.current.click();
+        setNote({ eid : currentNote._id,etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag });
+        console.log(note.eid+note.etitle);
+    }
+
+    const onChange = (e) => {
+        setNote({ ...note, [e.target.name]: e.target.value });// notes ko hi update kar dega(... ka matlab) 
+    }
+
+    const updatingNote = (e) => {
+        updateNote(note.eid, note.etitle, note.edescription, note.etag);
+    }
     return (
         <>
-        <div className="col-md-2 my-3">
-            <p>
-                <button className="btn" style={{backgroundColor: "#7532F9", color : "white"}} type="button" data-bs-toggle="collapse" data-bs-target={`#collapseExample-${props.note_number-1}`} aria-expanded="false" aria-controls={`collapseExample-${props.note_number-1}`}>
-                    {notes[props.note_number-1].title}
-                </button>
-            </p>
-            <div className="collapse" id={`collapseExample-${props.note_number-1}`}>
-                <div className="card card-body">
-                    {notes[props.note_number-1].description}
-                    <div className="d-flex flex-row">
-                        <i className="bi bi-trash3 mx-1 my-1" onClick={()=>{deleteNote(notes[props.note_number-1]._id)}} style={{cursor : 'pointer'}}></i>
-                        <i className="bi bi-pencil-square mx-1 my-1" style={{cursor : 'pointer'}}></i>
+            <button type="button" ref={modal_ui} className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+            </button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" >
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form style={{ width: "90%" }}>
+                                <div className="mb-3">
+                                    <label htmlFor="title" className="form-label"><strong>Title</strong></label>
+                                    <input type="text" onChange={onChange} className="form-control" id="etitle" value={note.etitle} name='etitle'/>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="description" className="form-label"><strong>Description</strong></label>
+                                    <input type="text" onChange={onChange} className="form-control" value={note.edescription} id="edescription" name='edescription' />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="tag" className="form-label"><strong>Tag</strong></label>
+                                    <input type="text" onChange={onChange} className="form-control" value={note.etag} id="etag" name='etag' />
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" onClick={updatingNote} style={{ backgroundColor: "#7532F9", color: "white" }} className="btn btn-primary">Update</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className="col-md-2 my-3">
+                <p>
+                    <button className="btn" style={{ backgroundColor: "#7532F9", color: "white" }} type="button" data-bs-toggle="collapse" data-bs-target={`#collapseExample-${props.note_number - 1}`} aria-expanded="false" aria-controls={`collapseExample-${props.note_number - 1}`}>
+                        {notes[props.note_number - 1].title}
+                    </button>
+                </p>
+                <div className="collapse" id={`collapseExample-${props.note_number - 1}`}>
+                    <div className="card card-body">
+                        {notes[props.note_number - 1].description}
+                        <div className="d-flex flex-row">
+                            <i className="bi bi-trash3 mx-1 my-1" onClick={() => { deleteNote(notes[props.note_number - 1]._id) }} style={{ cursor: 'pointer' }}></i>
+                            <i className="bi bi-pencil-square mx-1 my-1" onClick={() => { openModal(notes[props.note_number - 1]) }} style={{ cursor: 'pointer' }}></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -84,15 +84,18 @@ const NoteState = (props) => {
     const json = await response.json();
     console.log(json);
 
+    let newNotes=JSON.parse(JSON.stringify(notes));
     // Logic to update
-    for (let index = 0; index < notes.length; index++) {
-      const element = notes[index];
+    for (let index = 0; index < newNotes.length; index++) {
+      const element = newNotes[index];
       if (element._id === id) {
-        element.title = title;
-        element.description = description;
-        element.tag = tag;
+        newNotes[index].title = title;
+        newNotes[index].description = description;
+        newNotes[index].tag = tag;
+        break;
       }
     }
+    setNotes(newNotes);
   }
 
 
